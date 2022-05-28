6558bc42aa32babcf4cafb7a356a3c2616806790
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-28 12:47:55 +0530

    Color array added and appended on ui

--- syngentareact/src/Components/Home/Home.jsx
+++ syngentareact/src/Components/Home/Home.jsx
@@ -2,11 +2,45 @@ import React from 'react';
 import "./Home.css";
 
 export default function Home() {
+    const ballon = [
+        {
+            color: "blue",
+            index: 0
+        },
+        {
+            color: "brown",
+            index: 1
+        },
+        {
+            color: "green",
+            index: 2
+        },
+        {
+            color: "pink",
+            index: 3
+        },
+        {
+            color: "voilet",
+            index: 4
+        },
+
+    ]
+
+
     return (
         <div className='HomeMain'>
             <div className='ColorMainDiv'>
                 <div>Empty</div>
-                <div>Color</div>
+                <div className='ColorCircleMain'>
+
+                    {
+                        ballon.map((e) => (
+                            <div className='IndividualCircle' style={{ background: `${e.color}` }}>{e.index}</div>
+                        ))
+                    }
+
+
+                </div>
             </div>
 
             <div className='InputMainDiv'>
