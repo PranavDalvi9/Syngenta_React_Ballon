2d788ed8efe85123a87fd29d5034437b2a9a23c7
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-28 12:25:10 +0530

    Home UI Created

--- syngentareact/src/App.js
+++ syngentareact/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Components/Home/Home"
 
 function App() {
   return (
     <div className="App">
       <h1>syngenta</h1>
+      <Home/>
     </div>
   );
 }
--- syngentareact/src/Components/Home/Home.jsx
+++ syngentareact/src/Components/Home/Home.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import "./Home.css";
 
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='HomeMain'>
+            <div className='ColorMainDiv'>
+                <div>Empty</div>
+                <div>Color</div>
+            </div>
+
+            <div className='InputMainDiv'>
+                <input type="text" placeholder='Add Number to shoot' /> <br /><br />
+                <button>Shoot</button>
+            </div>
+        </div>
+    )
 }
