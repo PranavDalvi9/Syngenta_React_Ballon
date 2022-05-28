1f96bf7753cf9f9dcceff4c7fff21e17a97c0a21
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-28 13:52:47 +0530

    Onclick on empty div circle return to main div function added

--- syngentareact/src/Components/Home/Home.jsx
+++ syngentareact/src/Components/Home/Home.jsx
@@ -25,52 +25,60 @@ export default function Home() {
         },
     ]
 
-    const [ballonData , setBallonData] = useState([])
+    const [ballonData, setBallonData] = useState([])
 
 
-    const [numberShoot , setNumberShoot] = useState(0);
+    const [numberShoot, setNumberShoot] = useState(0);
 
-    const[emptyBag , setEmptyBag] = useState([])
+    const [emptyBag, setEmptyBag] = useState([])
 
     useEffect(() => {
         setBallonData(ballon11)
-    },[])
+    }, [])
 
     const handleShoot = () => {
-        console.log("shoot Number" , numberShoot)
+        console.log("shoot Number", numberShoot)
         // console.log("number choosen" ,ballonData[numberShoot] )
         const findIndiv = ballonData.find((e) => e.index === +numberShoot)
         // console.log("datat find after shoot" , findIndiv)
-        setEmptyBag([...emptyBag, findIndiv ])
+        setEmptyBag([...emptyBag, findIndiv])
 
-        const afteradd = ballonData.filter((e) => findIndiv.index !== e.index )
+        const afteradd = ballonData.filter((e) => findIndiv.index !== e.index)
         setBallonData(afteradd)
         // console.log("after filter" , afteradd)
     }
-    console.log("emptybaggg" ,emptyBag.length, emptyBag)
+    // console.log("emptybaggg", emptyBag.length, emptyBag)
 
+    const handleCircleclicked = (e) => {
+        console.log("circle clicked",e.index)
+        setBallonData([...ballonData,e])
+        console.log("baggg" , emptyBag)
+        const fillll =  emptyBag.filter((x) => e.index !== x.index)
+        setEmptyBag(fillll)
+        // console.log("filll" , fillll)
+    }
 
     return (
         <div className='HomeMain'>
             <div className='ColorMainDiv'>
                 <div>
                     {
-                        emptyBag.length >0 ? 
-                        <>
-                        {
-                        emptyBag.map((e) => (
-                            <div className='IndividualCircle' style={{ background: `${e.color}` }} key={e.index}>{e.index}</div>
-                        ))
-                    }
-                        </> : 
-                        <>no</>
+                        emptyBag.length > 0 ?
+                            <>
+                                {
+                                    emptyBag.map((e) => (
+                                        <div className='IndividualCircle' style={{ background: `${e.color}` }} key={e.index} onClick={() =>handleCircleclicked(e)} >{e.index}</div>
+                                    ))
+                                }
+                            </> :
+                            <>no</>
                     }
                 </div>
                 <div className='ColorCircleMain'>
 
                     {
                         ballonData.map((e) => (
-                            <div className='IndividualCircle' style={{ background: `${e.color}` }} key={e.index}>{e.index }</div>
+                            <div className='IndividualCircle' style={{ background: `${e.color}` }} key={e.index}>{e.index}</div>
                         ))
                     }
 
@@ -79,7 +87,7 @@ export default function Home() {
             </div>
 
             <div className='InputMainDiv'>
-                <input type="text" placeholder='Add Number to shoot' value={numberShoot} onChange={(e) => setNumberShoot(e.target.value)}/> <br /><br />
+                <input type="text" placeholder='Add Number to shoot' value={numberShoot} onChange={(e) => setNumberShoot(e.target.value)} /> <br /><br />
                 <button onClick={() => handleShoot()}>Shoot</button>
             </div>
         </div>
